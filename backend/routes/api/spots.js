c9e05af38147e30ddf737d2935677763fc3f6c7d
Landon <landon.hurst@outlook.com>
2023-12-02 14:26:38 -0500

    get spot by id finished

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { Spot, Review, SpotImage } = require('../../db/models');
+const { Spot, Review, SpotImage, User, sequelize } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation.js');
 const spot = require('../../db/models/spot.js');
@@ -90,5 +90,57 @@ router.get('/current', requireAuth, async (req, res, next) => {
 
     return res.json(spotObj)
 });
+
+router.get('/:spotId', async (req, res, next) => {
+    const spot = await Spot.findByPk(req.params.spotId, {
+        include: [
+            {
+                model: Review
+            },
+            {
+                model: SpotImage,
+                attributes: ['id', 'url', 'preview']
+            },
+            {
+                model: User,
+                attributes: ['id', 'firstName', 'lastName']
+            }
+        ],
+    });
+    if (!spot) {
+        return res.status(404).json({
+            message: 'Spot couldn\'t be found'
+        });
+    } else {
+        let total = 0;
+        spot.Reviews.forEach(review => {
+            total += review.stars;
+        });
+
+        const spotObj = {
+            id: spot.id,
+            ownerId: spot.ownerId,
+            address: spot.address,
+            city: spot.city,
+            state: spot.state,
+            country: spot.country,
+            lat: spot.lat,
+            lng: spot.lng,
+            name: spot.name,
+            description: spot.description,
+            price: spot.price,
+            createdAt: spot.createdAt,
+            updatedAt: spot.updatedAt,
+            numReviews: await spot.countReviews(),
+            avgStarRating: total / spot.Reviews.length,
+            SpotImages: spot.SpotImages,
+            Owner: spot.User
+        }
+
+        return res.json(spotObj);
+    }
+});
+
+
 
 module.exports = router;
