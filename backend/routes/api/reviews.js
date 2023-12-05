ba9003c9b335ff55579af3d2a83a61ebad13dbfd
Landon <landon.hurst@outlook.com>
2023-12-04 19:57:20 -0500

    add image to review by review id route finished

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -7,6 +7,23 @@ const { handleValidationErrors } = require('../../utils/validation.js');
 
 const router = express.Router();
 
+const reviewAuthorize = async (req, res, next) => {
+    const { user } = req;
+    const review = await Review.findByPk(req.params.reviewId);
+
+    if (!review) {
+        return res.status(404).json({
+            message: 'Review couldn\'t be found'
+        });
+    } else if (user.id !== review.userId) {
+        return res.status(403).json({
+            message: 'Forbidden'
+        });
+    } else {
+        next();
+    }
+};
+
 router.get('/current', requireAuth, async (req, res, next) => {
     const { user } = req;
 
@@ -56,6 +73,36 @@ router.get('/current', requireAuth, async (req, res, next) => {
     reviewObj.Reviews = reviewsList;
 
     return res.json(reviewObj);
+});
+
+router.post('/:reviewId/images', requireAuth, reviewAuthorize, async (req, res, next) => {
+    const { url } = req.body;
+    const review = await Review.findByPk(req.params.reviewId, {
+        include: [
+            {
+                model: ReviewImage
+            }
+        ]
+    });
+
+    const max = await review.countReviewImages();
+
+    if (max >= 10) {
+        return res.status(403).json({
+            message: 'Maximum number of images for this resource was reached'
+        });
+    }
+
+    const imageObj = {};
+    const newImage = await ReviewImage.create({
+        reviewId: review.id,
+        url
+    });
+
+    imageObj.id = newImage.id;
+    imageObj.url = newImage.url;
+
+    return res.json(imageObj);
 });
 
 module.exports = router;
