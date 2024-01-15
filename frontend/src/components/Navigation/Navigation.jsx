3cdc99f5664dcde70f8d152201e8467e63db38b0
Landon <landon.hurst@outlook.com>
2024-01-15 15:58:39 -0500

    profile dropdown toggle implemented

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,6 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import './Navigation.css';
 
 function Navigation({ isLoaded }) {
     const user = useSelector(state => state.session.user);
