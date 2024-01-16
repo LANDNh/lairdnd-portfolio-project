6b341d3570c0a9db94d598f54cdc3a0cf865436b
Landon <landon.hurst@outlook.com>
2024-01-15 21:22:13 -0500

    login/signup modal buttons converted to li elements

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,41 +1,21 @@
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import OpenModalButton from '../OpenModalButton/OpenModalButton';
-import LoginFormModal from '../LoginFormModal';
-import SignupFormModal from '../SignupFormModal';
 import './Navigation.css';
 
 function Navigation({ isLoaded }) {
     const user = useSelector(state => state.session.user);
 
-    const sessionLinks = user ? (
-        <li className='prof-button'>
-            <ProfileButton user={user} />
-        </li>
-    ) : (
-        <>
-            <li>
-                <OpenModalButton
-                    buttonText='Log In'
-                    modalComponent={<LoginFormModal />}
-                />
-            </li>
-            <li>
-                <OpenModalButton
-                    buttonText='Sign Up'
-                    modalComponent={<SignupFormModal />}
-                />
-            </li>
-        </>
-    );
-
     return (
-        <ul className='navbar'>
-            <li className='home-link'>
+        <ul>
+            <li>
                 <NavLink to="/">Home</NavLink>
             </li>
-            {isLoaded && sessionLinks}
+            {isLoaded && (
+                <li>
+                    <ProfileButton user={user} />
+                </li>
+            )}
         </ul>
     );
 }
--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,11 +1,19 @@
 import { useState, useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
+import OpenModalMenuItem from './OpenModalMenuItem';
+import LoginFormModal from '../LoginFormModal';
+import SignupFormModal from '../SignupFormModal';
 
 function ProfileButton({ user }) {
     const dispatch = useDispatch();
     const [showMenu, setShowMenu] = useState(false);
     const ulRef = useRef();
+
+    const toggleMenu = (e) => {
+        e.stopPropagation();
+        setShowMenu(!showMenu);
+    };
 
     useEffect(() => {
         if (!showMenu) return;
@@ -20,14 +28,12 @@ function ProfileButton({ user }) {
         return () => document.removeEventListener('click', closeMenu);
     }, [showMenu]);
 
+    const closeMenu = () => setShowMenu(false);
+
     const logout = (e) => {
         e.preventDefault();
         dispatch(sessionActions.logoutUser());
-    };
-
-    const toggleMenu = (e) => {
-        e.stopPropagation();
-        setShowMenu(!showMenu);
+        closeMenu();
     };
 
     const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
@@ -38,12 +44,33 @@ function ProfileButton({ user }) {
                 <i className="fas fa-user-circle" />
             </button>
             <ul className={ulClassName} ref={ulRef}>
-                <li>{user.username}</li>
-                <li>{user.firstName} {user.lastName}</li>
-                <li>{user.email}</li>
-                <li>
-                    <button onClick={logout}>Log Out</button>
-                </li>
+                {user ? (
+                    <>
+                        <li>{user.username}</li>
+                        <li>{user.firstName} {user.lastName}</li>
+                        <li>{user.email}</li>
+                        <li>
+                            <button onClick={logout}>Log Out</button>
+                        </li>
+                    </>
+                ) : (
+                    <>
+                        <li>
+                            <OpenModalMenuItem
+                                itemText="Log In"
+                                modalComponent={<LoginFormModal />}
+                                onItemClick={closeMenu}
+                            />
+                        </li>
+                        <li>
+                            <OpenModalMenuItem
+                                itemText="Sign Up"
+                                modalComponent={<SignupFormModal />}
+                                onItemClick={closeMenu}
+                            />
+                        </li>
+                    </>
+                )}
             </ul>
         </>
     );
